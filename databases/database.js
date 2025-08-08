214e5c32cba408d8d991fa449ab00324644124a2
Pedro Renan <pedrorenanmesquitabrasil@gmail.com>
2025-08-08 20:31:38 -0300

    Atualiza configuração do Sequelize para conexão com Render PostgreSQL

--- databases/database.js
+++ databases/database.js
@@ -1,9 +1,14 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('guiaperguntas', 'admin', 'xR9519mwRvFWTw4lysciqoPsCtKB6Evd', {
-  host: 'dpg-d2b123.render.com', // substitua pelo seu host exato
-  dialect: 'postgres',
-  port: 5432,
+    host: 'dpg-d2b8156r433s739htmv0-a.oregon-postgres.render.com',
+    port: 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+        require: true,          // SSL obrigatório no Render
+        rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
-
